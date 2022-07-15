69db47ab766b60bba7cb5b56e0f254c84ec0d637
d0r4q <sunlightflat_yama@gmail.com>
2022-07-15 17:56:13 +0900

    test 20220715

---
+++ resources/views/folders/create.blade.php
@@ -0,0 +1,36 @@
+@extends('layout')
+
+@section('content')
+        <div class="container">
+            <div class="row">
+                <div class="col col-md-offset-3 col-md-6">
+                    <nav class="panel panel-default">
+                        <div class="panel-heading">フォルダを追加する</div>
+
+                        <div class="panel-body">
+                            @if($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach($errors->all() as $message)
+                                    <li>{{ $message }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                            @endif
+                            <form action="{{ route('folders.create') }}" method="post">
+                                @csrf
+                                <div class="form-group">
+                                    <label for="title">フォルダ名</label>
+                                    <input type="text" id="title" class="form-control" name="title" value="{{ old('title') }}"/>
+                                </div>
+
+                                <div class="text-right">
+                                    <button class="btn btn-primary">送信</button>
+                                </div>
+                            </form>
+                        </div>  
+                    </nav>
+                </div>
+            </div>
+        </div>
+@endsection
